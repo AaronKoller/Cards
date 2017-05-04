a74797447e3e9bafe98cd44ec432e91e23f286b5
Aaron Koller <aaronkoller777@gmail.com>
2017-05-03 23:17:04 -0700

    Cleaned up the ICard Interface.

--- Cards/ICard.cs
+++ Cards/ICard.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cards
+﻿namespace Cards
 {
-    public interface ICardSuitValue<out S, V>
+    public interface ICardSuitValue<out S, out V>
     {
         S Suit { get; }
 
