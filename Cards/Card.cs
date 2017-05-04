4228f2e893004bf7e7c33843a77b127847978bc9
Aaron Koller <aaronkoller777@gmail.com>
2017-05-03 22:33:32 -0700

    Added the Card Class

---
+++ Cards/Card.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Cards
+{
+    class Card
+    {
+        public Card(Suit suit, CardValue cardValue)
+        {
+            Suit = suit;
+            CardValue = cardValue;
+        }
+
+        public Suit Suit { get; }
+
+        public CardValue CardValue { get; }
+    }
+
+    public enum CardValue
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+        Ace = 14
+    }
+
+    public enum Suit
+    {
+        Hearts = 1,
+        Spades = 2,
+        Clubs = 3,
+        Diamonds = 4
+    }
+
+}
